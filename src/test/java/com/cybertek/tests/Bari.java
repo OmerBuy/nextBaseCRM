7a6bfeab6fa245b6d1113f39206994d1636fe683
Kerim Goztepe <kerimgoztepe@yahoo.com>
2020-05-23 19:15:13 +0300

    new version

--- src/test/java/com/cybertek/tests/Bari.java
+++ src/test/java/com/cybertek/tests/Bari.java
@@ -3,7 +3,7 @@ package com.cybertek.tests;
 public class Bari {
     public static void main(String[] args) {
         System.out.println("BARI BARI LEICESTER");
-        System.out.println("BARI BARI LEICESTER");
-        System.out.println("BARI BARI LEICESTER");
+        System.out.println("MILANO BARI LEICESTER");
+        System.out.println("LONDON EDINBURG LEICESTER");
     }
 }
