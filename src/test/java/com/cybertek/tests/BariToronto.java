5dfb9e979e872abea09c48eb1cbc913eb75db413
Kerim Goztepe <kerimgoztepe@yahoo.com>
2020-05-23 19:53:21 +0300

    new version

--- src/test/java/com/cybertek/tests/BariToronto.java
+++ src/test/java/com/cybertek/tests/BariToronto.java
@@ -1,6 +1,6 @@
 package com.cybertek.tests;
 
-public class Bari {
+public class BariToronto {
     public static void main(String[] args) {
         System.out.println("BARI BARI LEICESTER");
     }
