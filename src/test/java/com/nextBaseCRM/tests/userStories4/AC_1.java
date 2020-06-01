86cc7817a2214a484ae6d3395023aeec1897f69b
CCicek <muhammetcicekster@gmail.com>
2020-06-01 23:09:14 +0000

    updating config.

--- src/test/java/com/nextBaseCRM/tests/userStories4/AC_1.java
+++ src/test/java/com/nextBaseCRM/tests/userStories4/AC_1.java
@@ -1,4 +1,18 @@
 package com.nextBaseCRM.tests.userStories4;
 
-public class AC_1 {
+import com.nextBaseCRM.pages.LoginPage;
+import com.nextBaseCRM.tests.TestBase;
+import org.testng.annotations.Test;
+
+public class AC_1 extends TestBase {
+
+    @Test
+    public void tc1() {
+        new LoginPage().loginAsHR();
+
+
+
+
+
+    }
 }
