dd150fcd3a00bb58bb66f7307d86eccb15633b50
vatanhorasan <vatanhorasan@gmail.com>
2020-06-01 22:00:37 -0500

    updated Test_US6_AC1.java

--- src/test/java/com/nextBaseCRM/tests/userStory6/Test_US6_AC1.java
+++ src/test/java/com/nextBaseCRM/tests/userStory6/Test_US6_AC1.java
@@ -4,17 +4,58 @@ import com.nextBaseCRM.pages.AppreciationPage;
 import com.nextBaseCRM.pages.LoginPage;
 import com.nextBaseCRM.tests.TestBase;
 import com.nextBaseCRM.utilities.BrowserUtils;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Test_US6_AC1 extends TestBase {
 
     @Test
-    public void test(){
+    public void uploadFilesFromPCTest(){
 
-        new LoginPage().loginAsHR();
+        extentLogger = report.createTest("Upload Files From PC Test");
+        extentLogger.info("Login as Marketing");
+        new LoginPage().loginAsMarketing();
+
         AppreciationPage appreciationPage = new AppreciationPage();
-        appreciationPage.navigateToModule("Activity Stream" , "Appreciation");
 
-        BrowserUtils.waitFor(5);
+        extentLogger.info("Send Capture File");
+        appreciationPage.uploadFilesFromPC("src/test/resources/Capture.PNG");
+        BrowserUtils.waitFor(2);
+
+        WebElement fileElement = appreciationPage.getUploadFileElement("Capture");
+        Assert.assertTrue(fileElement.isDisplayed());
     }
+
+    @Test
+    public void uploadFilesFromExternalDriveTest(){
+
+        extentLogger = report.createTest("Selecting Files from External device");
+
+        extentLogger.info("Login as Marketing");
+        new LoginPage().loginAsMarketing();
+
+        AppreciationPage appreciationPage = new AppreciationPage();
+        appreciationPage.uploadFilesFromExternalDrive("Some address");
+
+        extentLogger.pass("PASSED: External Drive");
+    }
+
+    @Test
+    public void uploadFilesFromBitrix24Test(){
+
+        extentLogger = report.createTest("Uploading Files from Bitrix24");
+
+        extentLogger.info("Login as Marketing");
+        new LoginPage().loginAsMarketing();
+
+        AppreciationPage appreciationPage = new AppreciationPage();
+        appreciationPage.uploadFilesFromBitrix24("Capture");
+
+        extentLogger.pass("PASSED: Upload from Bitrix24");
+    }
+
+
+
+
 }
