5cc8298c4e002f26707a639792a5573cade04229
TARIKERKAM <tedogan@hotmail.com>
2020-06-06 13:41:11 +0400

    dene

--- src/test/java/com/nextBaseCRM/pages/Announcement.java
+++ src/test/java/com/nextBaseCRM/pages/Announcement.java
@@ -6,6 +6,13 @@ import org.openqa.selenium.support.FindBy;
 
 public class Announcement extends BasePage {
 
+
+    @FindBy(css = "#bx-b-uploadfile-blogPostForm")
+    public WebElement uploadFilesIcon;
+
+    @FindBy(xpath = "//span[.='Select document from Bitrix24']")
+    public WebElement selectFromBitrix;
+
     @FindBy(xpath = "(//span[@id='feed-add-post-form-link-text'])")
     public WebElement moreButton;
 
--- src/test/java/com/nextBaseCRM/tests/UserStory7/Test_US7_AC6.java
+++ src/test/java/com/nextBaseCRM/tests/UserStory7/Test_US7_AC6.java
@@ -1,13 +1,66 @@
 package com.nextBaseCRM.tests.UserStory7;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import com.nextBaseCRM.pages.Announcement;
+import com.nextBaseCRM.pages.AppreciationPage;
+import com.nextBaseCRM.pages.LoginPage;
+import com.nextBaseCRM.tests.TestBase;
 import org.testng.annotations.Test;
 
-import java.util.List;
-
-public class Test_US7_AC6 {
+public class Test_US7_AC6 extends TestBase {
 
 
+    @Test
+
+    public void uploadFileTest() {
+        new LoginPage().loginAsMarketing();
+
+       // AppreciationPage appreciationPage=new AppreciationPage();
+       // appreciationPage.navigateToModule("Activity Stream","Announcement");
+        //AnnouncementPage announcementPage=new AnnouncementPage();
+        //announcementPage.uploadFilesIcon.click();
+        //announcementPage.selectFromBitrix.click();
+
+        AppreciationPage appreciationPage=new AppreciationPage();
+        appreciationPage.navigateToModule("Activity Stream","Announcement");
+        Announcement announcementPage1=new Announcement();
+        announcementPage1.uploadFilesIcon.click();
+        announcementPage1.selectFromBitrix.click();
+
+
+
+
+        //announcementPage.moreButton.click();
+
+
+        //WebElement dropdownElement = driver.findElement(By.id("dropdownMenuLink"));
+       // WebElement dropdownElement = driver.findElement(By.cssSelector("#dropdownMenuLink"));
+       // dropdownElement.click();
+
+        //List<WebElement> dropdownOptions= driver.findElements(By.cssSelector("a.dropdown-item"));
+
+        //List<WebElement> more = driver.findElements(By.xpath ("//span[@id='feed-add-post-form-link-text']"));
+
+       // for (WebElement option : more) {
+         //   System.out.println(option.getText());
+        //}
+        // for (WebElement option : dropdownOptions) {
+        //    System.out.println(option.getText());
+        // }
+
+        //click yahoo
+       // more.get(2).click();
+
+
+
+
+
+
+        //announcementPage.AnnouncementButton.click();
+
+
+    }
 }
+
+
+
+
