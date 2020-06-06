b40e8ff9aac9808c74b117067c23e92c82d2f079
Kerim Goztepe <kerimgoztepe@yahoo.com>
2020-06-06 16:41:39 +0300

    new files

---
+++ src/test/java/com/nextBaseCRM/tests/userStory7/TestMrTarik.java
@@ -0,0 +1,11 @@
+package com.nextBaseCRM.tests.userStory7;
+
+public class TestMrTarik {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 5;
+        int c = a*b;
+
+        System.out.println(c);
+    }
+}
